185ba82be4a55005633301f01ce9f41f9af940f6
remyqpham <remyqpham@gmail.com>
2017-08-17 16:03:49 -0700

    encoders properly push a value from 0-127, barring multiple serial inputs from the encoders due to multiple inputs per 'notch' of the enc.  next step is to push MIDI

--- microMIDI_1.0/microMIDI_1.0.ino
+++ microMIDI_1.0/microMIDI_1.0.ino
@@ -10,15 +10,16 @@
 #include <SPI.h>
 
 #define NUM_TLC59711 1
-#define NUM_BUTTONS 4
+#define NUM_BUTTONS 6
+#define NUM_ENCODERS 3 //to be changed to four later
 #define DEBOUNCE 50
 
 #define data 16
 #define clock 15
 
-const int ButtonPin [NUM_BUTTONS] = {9,8,7,6};
+const int ButtonPin [NUM_BUTTONS] = {9,8,7,6, 5, 10};
 
-int buttonState [NUM_BUTTONS]; //1 is little button, 2+3+4 are encoders for now
+int buttonState [NUM_BUTTONS]; //1 is little button, 2+3+4 are encoders for now, 5 is bank navigation (up one) and 6 is bank navi (down one)
 
 Adafruit_TLC59711 tlc = Adafruit_TLC59711(1, clock, data);
 
@@ -30,11 +31,10 @@ Encoder myEncA(1, 14);
 Encoder myEncB(0, A0);
 Encoder myEncC(2,4);
 //   avoid using pins with LEDs attached
-
 char str[50];
-long oldPositionA  = -999;
-long oldPositionB  = -999;
-long oldPositionC  = -999;
+
+
+long oldPosition [NUM_ENCODERS];
 
 long oldTime[NUM_BUTTONS];
 long newTime[NUM_BUTTONS];
@@ -50,6 +50,13 @@ void setup() {
     pinMode(ButtonPin[i], INPUT);
   }
 
+  
+  for(int i=0; i<NUM_ENCODERS; i++){
+    oldPosition[i]=255;//set the encoder to begin on startup in the middle of the range
+    writeEncoder(i, 255);
+  }
+
+
   tlc.begin();
   tlc.write();
 }
@@ -59,27 +66,32 @@ void loop() {
   for (int i=0; i<NUM_BUTTONS; i++){
     buttonState[i]=digitalRead(ButtonPin[i]);
   }
-   
-  long newPositionA = myEncA.read();
-  long newPositionB = myEncB.read();
-  long newPositionC = myEncC.read();
-  if (newPositionA != oldPositionA && newPositionA>=0 && newPositionA <= 255) {
-    oldPositionA = newPositionA;
-    sprintf(str,"A:%d\n",newPositionA);
-    colorWipe(pow(newPositionA,1.5),0,0,5);
-    Serial.println(str);
-  }
-  if (newPositionB != oldPositionB && newPositionB>=0 && newPositionB <= 255) {
-    oldPositionB = newPositionB;
-    sprintf(str,"B:%d\n",newPositionB);
-    colorWipe(0,pow(newPositionB,1.5),0,5);
-    Serial.println(str);
-  }
-  if (newPositionC != oldPositionC && newPositionC>=0 && newPositionC <= 255) {
-    oldPositionC = newPositionC;
-    sprintf(str,"C:%d\n",newPositionC);
-    colorWipe(0,0,pow(newPositionC,1.5),5);
-    Serial.println(str);
+
+  long newPosition [NUM_ENCODERS];
+  
+  newPosition[0] = myEncA.read();
+  newPosition[1] = myEncB.read();
+  newPosition[2] = myEncC.read();    
+
+  for(int i=0;i<NUM_ENCODERS;i++){
+    if(newPosition[i]<0){
+      newPosition[i]=0;
+      //newPosition[i]=511;
+      writeEncoder(i,0);
+    }else if(newPosition[i]>511){
+      newPosition[i]=511;
+      //newPosition[i]=0;
+      writeEncoder(i,511);
+    }
+    
+    if (newPosition[i] != oldPosition[i]) {
+      oldPosition[i] = newPosition[i];
+      sprintf(str,"%d:%d\n",i,(511-oldPosition[i])/4);//
+      //colorWipe(pow(newPosition[i],1.5),0,0,5);
+      Serial.println(str);
+    }
+
+
   }
 
   //MIDI
@@ -102,7 +114,7 @@ void buttonInput(int b){
   if(buttonState[b]==HIGH && noteSend[b]==0 && newTime[b]-oldTime[b]>DEBOUNCE){
     oldTime[b] = newTime[b];
     midiEventPacket_t noteOn = {0x09, 0x90 | 0, 79+b, 64};  //stupid test  
-    //midiEventPacket_t noteOn = {0x0B, 0xB0 | 0, 80+b, 127};
+    //midiEventPacket_t noteOn = {0x0B, 0xB0 | 0, 80+b, 127}; // general purpose
     Serial.println("Sending note on");
     MidiUSB.sendMIDI(noteOn); //sending channel0, G4, norm. velocity
     MidiUSB.flush();
@@ -110,7 +122,7 @@ void buttonInput(int b){
     
   }else if(buttonState[b]==LOW && noteSend[b]==1 && newTime[b]-oldTime[b]>DEBOUNCE){
     midiEventPacket_t noteOff = {0x08, 0x80 | 0, 79+b, 64};
-    //midiEventPacket_t noteOff = {0x0B, 0xB0 | 0, 80+b, 0};
+    //midiEventPacket_t noteOff = {0x0B, 0xB0 | 0, 80+b, 0}; //general purpose
     Serial.println("Sending note off");
     MidiUSB.sendMIDI(noteOff);
     MidiUSB.flush();
@@ -119,4 +131,23 @@ void buttonInput(int b){
   }
    
 }
+
+void writeEncoder(int i, int val){
+  switch(i){
+    case 0: 
+      myEncA.write(val);
+      break;
+    case 1: 
+      myEncB.write(val); 
+      break;
+    case 2: 
+      myEncC.write(val);
+      break;
+    default:
+      break;
+  }
+
+  return;
+}
+
 
